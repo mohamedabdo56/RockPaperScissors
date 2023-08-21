15ad901d83f93424830fb364bdee5e466df4a7ff
mohamed abdo <mohammed.ibab93@gmail.com>
2023-08-21 14:43:28 +0300

    added js logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  return options[choice];
+}
+function checkWinner() {
+  if (playerSelection === computerSelection) {
+    console.log("Draw");
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    console.log("Lose");
+  } else console.log("win");
+}
+function playRound(playerSelection, computerSelection) {
+  checkWinner();
+  console.log(playerSelection, computerSelection);
+}
+let playerSelection = prompt("what is your choice ?");
+let computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
